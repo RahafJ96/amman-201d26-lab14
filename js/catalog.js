8ded652bd4ee5949ac1d656285b2e51b62fc7c60
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-06-09 17:03:48 +0300

    edit the catalog and cart

--- js/catalog.js
+++ js/catalog.js
@@ -14,6 +14,7 @@ function populateForm() {
   for (let i in Product.allProducts) {
     let optionTag=document.createElement('option');
     selectElement.appendChild(optionTag)
+    //????
     optionTag.textContent=Product.allProducts[i].name;
 
 
@@ -40,19 +41,37 @@ function handleSubmit(event) {
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  
+  //??????????
   // TODO: suss out the item picked from the select list
+  let item = document.getElementById('items').value;
   // TODO: get the quantity
+  let quantity= document.getElementById('quantity').value;
+  console.log(item,quantity);
   // TODO: using those, add one item to the Cart
+  cart.addItem(item,quantity);
 }
-
+// let counter=0;
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+ let counterValue =document.getElementById('itemCount');
+ counterValue.textContent = '';
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
+  let item=document.getElementById('item').value;
+  let quantity=document.getElementById('quantity').value;
+  // let arrayString= JSON.stringify(Cart.items);
+
+  // localStorage.setItem('cart',arrayString);
+
   // TODO: Get the item and quantity from the form
+  
+  
   // TODO: Add a new element to the cartContents div with that information
+  let cartContents = document.getElementById('cartContents');
+  cartContents.appendChild(listElement);
+  listElement.textContent= ` item: ${it} and Quantity: ${quantity} `;
 }
 
 // Set up the "submit" event listener on the form.
